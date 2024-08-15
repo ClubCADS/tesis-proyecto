d5cf5d7ecbbe56d213f0ac30638c88e3455407e4
ClubCADS <clubcads@gmail.com>
2024-08-15 10:37:54 -0300

    delete notes

--- app/notes/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { createClient } from '@/utils/supabase/server';
-
-export default async function Notes() {
-  const supabase = createClient();
-  const { data: notes } = await supabase.from("survey_responses").select();
-
-  return <pre>{JSON.stringify(notes, null, 2)}</pre>
-}
