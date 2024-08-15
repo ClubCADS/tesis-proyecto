a5f91f2abffd810ca40a070afdb71c10375336eb
ClubCADS <clubcads@gmail.com>
2024-08-15 10:52:30 -0300

    render en servidor

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,28 @@
-import dynamic from 'next/dynamic';
+// import dynamic from 'next/dynamic';
 
-const DynamicSurveyComponent = dynamic(() => import('@/components/SurveyComponent'), {
-  ssr: false,
-});
+// const DynamicSurveyComponent = dynamic(() => import('@/components/SurveyComponent'), {
+//   ssr: false,
+// });
 
-export default function Index() {
-  return (
-    <div className="w-full flex flex-col items-center">
-      <DynamicSurveyComponent />
-    </div>
-  );
-}
-
-
-// import SurveyComponent from "@/components/SurveyComponent";
-
-// export default async function Index() {
-
-
+// export default function Index() {
 //   return (
 //     <div className="w-full flex flex-col items-center">
-
-//       <SurveyComponent></SurveyComponent>
-
+//       <DynamicSurveyComponent />
 //     </div>
 //   );
 // }
+
+
+import SurveyComponent from "@/components/SurveyComponent";
+
+export default async function Index() {
+
+
+  return (
+    <div className="w-full flex flex-col items-center">
+
+      <SurveyComponent></SurveyComponent>
+
+    </div>
+  );
+}
