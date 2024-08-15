c67b6bf152bada8ad84227374fb90beb2c914b05
ClubCADS <clubcads@gmail.com>
2024-08-15 10:12:51 -0300

    delete function tutorial page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import { createClient } from "@/utils/supabase/server";
 import SurveyComponent from "@/components/SurveyComponent";
 
 export default async function Index() {
-  const canInitSupabaseClient = () => {
-    // This function is just for the interactive tutorial.
-    // Feel free to remove it once you have Supabase connected.
-    try {
-      createClient();
-      return true;
-    } catch (e) {
-      return false;
-    }
-  };
+  // const canInitSupabaseClient = () => {
+  //   // This function is just for the interactive tutorial.
+  //   // Feel free to remove it once you have Supabase connected.
+  //   try {
+  //     createClient();
+  //     return true;
+  //   } catch (e) {
+  //     return false;
+  //   }
+  // };
 
-  const isSupabaseConnected = canInitSupabaseClient();
+  // const isSupabaseConnected = canInitSupabaseClient();
 
   return (
     <div className="w-full flex flex-col items-center">
